fce9023f1a557d0a523599790c3526b2647f31c3
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-26 18:50:09 +0300

    Add mongoose and connect it to mongoDB

--- server/app.js
+++ server/app.js
@@ -3,15 +3,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
-const app = express();
+const mongoDB = process.env.MONGODB_URI;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());

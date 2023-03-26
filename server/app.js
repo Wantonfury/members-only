69d8367f8ef39d21ebcdabe4381fa12d6a71ba3a
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-26 19:05:19 +0300

    Add dotenv and config

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -38,8 +39,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).send();
 });
 
 module.exports = app;

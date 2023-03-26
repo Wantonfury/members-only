897e550176a4d14947f16ac9926f36ecd16de170
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-26 18:56:36 +0300

    Change index route to posts route

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const indexRouter = require('./routes/index');
+const postsRouter = require('./routes/posts');
 const usersRouter = require('./routes/users');
 
 const mongoDB = process.env.MONGODB_URI;
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/posts', postsRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
---
+++ server/routes/posts.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('../controllers/postController');
+
+//router.get('/', postController.get);
+
+module.exports = router;

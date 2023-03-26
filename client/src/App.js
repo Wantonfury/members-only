c69f7fcc7d0148dcf821f2ef6a51f8c47f11691d
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-26 21:56:45 +0300

    Add user provider and user state
    Also added Header, PostList and Footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,30 @@
 import './styles/App.css';
 import ServerContext from "./contexts/ServerContext";
-import SignUpForm from './components/SignUpForm';
+import UserContext from './contexts/UserContext';
+import { useState } from "react";
+import Header from './components/Header';
+import Footer from "./components/Footer";
+import PostList from './components/PostList';
 
 function App() {
+  const [user, setUser] = useState({
+    loggedIn: false,
+    username: '',
+    member: false,
+    admin: false
+  });
+  
   return (
     <ServerContext.Provider value={process.env.REACT_APP_SERVER || "http://localhost:3000"}>
-      <div className="App">
-        <SignUpForm />
-      </div>
+      <UserContext.Provider value={{user, setUser}}>
+        
+        <div className="App">
+          <Header />
+          <PostList />
+          <Footer />
+        </div>
+        
+      </UserContext.Provider>
     </ServerContext.Provider>
   );
 }

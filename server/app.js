408acbf1cb9dc1cb716aacb791e28171a934c8cb
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-26 19:44:19 +0300

    Add cors package

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const postsRouter = require('./routes/posts');
 const usersRouter = require('./routes/users');
@@ -18,6 +19,7 @@ async function main() {
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));

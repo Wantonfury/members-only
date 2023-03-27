2ccb1b41d29fef9255a098f48c9620ff342d9ddb
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-27 03:43:52 +0300

    Add button styling and modal opening function

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,16 @@
 import "../styles/Header.css"
+import "../styles/Button.css";
 
 const Header = (props) => {
+  const openModal = (modal) => {
+    document.querySelector(`#${modal}`).style.display = 'block';
+  }
+  
   return (
     <div id="header">
       <p><span>Members</span><span>Only</span></p>
-      <button>Log In</button>
-      <button>Sign Up</button>
+      <button className="button" onClick={() => openModal('login')}>Log In</button>
+      <button className="button" onClick={() => openModal('signup')}>Sign In</button>
     </div>
   );
 }

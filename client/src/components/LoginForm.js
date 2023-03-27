f4dff04200e303dbe63522b7352a944b6b50a2db
Wantonfury <orzan.alexandru94@gmail.com>
2023-03-27 03:45:59 +0300

    Add modal styling

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,9 @@
-import '../styles/LoginForm.css';
+import '../styles/Button.css';
+import '../styles/Modal.css';
 import { useState, useContext } from "react";
 import ServerContext from '../contexts/ServerContext';
 import axios from "axios";
+import BtnClose from './BtnClose';
 
 const LoginForm = () => {
   const SERVER = useContext(ServerContext);
@@ -29,16 +31,31 @@ const LoginForm = () => {
     });
   }
   
+  const modalClose = (e) => {
+    const modalContainer = document.querySelector('#login');
+    const btnClose = document.querySelector('#login .btn-close');
+    
+    if (e.target === modalContainer || e.target === btnClose) modalContainer.style.display = 'none';
+  }
+  
   return (
-    <form action="" method="POST" onSubmit={handleSubmit}>
-      <label htmlFor="username">Username: </label>
-      <input type="text" name="username" placeholder="Username" onChange={handleChange} />
-      
-      <label htmlFor="password">Password: </label>
-      <input type="password" name="password" placeholder="Password" onChange={handleChange} />
-      
-      <button type="submit">Sign Up</button>
-    </form>
+    <div id="login" className="modal-container" onClick={modalClose}>
+      <form action="" method="POST" className="modal form" onSubmit={handleSubmit}>
+        <BtnClose onClick={modalClose} />
+        
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" placeholder="Username" pattern="[A-Za-z0-9]{3,}" title="Username must  be at least 3 characters." onChange={handleChange} required />
+        </div>
+        
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" placeholder="Password" pattern="[A-Za-z0-9]{8,}" title="Password must  be at least 8 characters." onChange={handleChange} required />
+        </div>
+        
+        <button type="submit" className="button button-wide">Log In</button>
+      </form>
+    </div>
   );
 }
 

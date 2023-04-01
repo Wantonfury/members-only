c457bf548f02dcfbb4b3c70a7e4126fa619b1b44
Wantonfury <orzan.alexandru94@gmail.com>
2023-04-01 21:10:08 +0300

    Add user state and various context providers

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,49 @@
 import './styles/App.css';
 import ServerContext from "./contexts/ServerContext";
-import UserContext from './contexts/UserContext';
-import { useState } from "react";
 import Header from './components/Header';
 import Footer from "./components/Footer";
 import PostList from './components/PostList';
-import SignUpForm from './components/SignUpForm';
-import LoginForm from './components/LoginForm';
+import { ModalProvider } from './contexts/ModalContext';
+import UserContext from './contexts/UserContext';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const defaultUser = {
+  username: '',
+  loggedIn: false,
+  member: false,
+  admin: false
+}
+
+const SERVER = process.env.REACT_APP_SERVER || "http://localhost:3000";
 
 function App() {
-  const [user, setUser] = useState({
-    loggedIn: false,
-    username: '',
-    member: false,
-    admin: false
-  });
+  const [user, setUser] = useState(defaultUser);
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(() => {
+    axios.get(`${SERVER}/users/relog`, { withCredentials: true })
+      .then(res => res.data)
+      .then(res => setUser({
+        username: res.username,
+        loggedIn: res.loggedIn,
+        member: res.member,
+        admin: res.admin
+      }))
+      .catch(err => console.log(err))
+      .finally(() => setLoading(false));
+  }, []);
   
   return (
-    <ServerContext.Provider value={process.env.REACT_APP_SERVER || "http://localhost:3000"}>
-      <UserContext.Provider value={{user, setUser}}>
-        
-        <div className="App">
-          <section>
-            <SignUpForm />
-            <LoginForm />
-          </section>
-          
-          <Header />
-          <PostList />
-          <Footer />
-        </div>
-        
+    <ServerContext.Provider value={SERVER}>
+      <UserContext.Provider value={{ user, setUser }}>
+        <ModalProvider>
+          <div className="App">
+            <Header loading={loading} />
+            <PostList />
+            <Footer />
+          </div>
+        </ModalProvider>
       </UserContext.Provider>
     </ServerContext.Provider>
   );

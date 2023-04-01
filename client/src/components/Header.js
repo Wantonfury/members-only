95695c460a4719d5a8eb55a4fec6b146ed8e7eee
Wantonfury <orzan.alexandru94@gmail.com>
2023-04-01 21:03:49 +0300

    Remove previous modal and add the new ModalContext
    Also add UserContext to handle button logic for when the user is logged

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,41 @@
 import "../styles/Header.css"
 import "../styles/Button.css";
+import UserContext from "../contexts/UserContext";
+import { useContext } from "react";
+import useLoader from "../hooks/useLoader";
+import { ModalContext } from "../contexts/ModalContext";
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
+import LogoutForm from "./LogoutForm";
 
 const Header = (props) => {
-  const openModal = (modal) => {
-    document.querySelector(`#${modal}`).style.display = 'block';
+  const { user } = useContext(UserContext);
+  const [{ getLoader }] = useLoader();
+  const { handleModal } = useContext(ModalContext);
+  
+  const buttons = () => {
+    if (props.loading) return getLoader();
+    
+    if (!user.loggedIn) {
+      return (
+        <>
+          <button className="button" onClick={() => handleModal(<LoginForm />)}>Log In</button>
+          <button className="button" onClick={() => handleModal(<SignUpForm />)}>Sign Up</button>
+        </>
+      );
+    }
+    
+    return (
+      <>
+      <button className="button" onClick={() => handleModal(<LogoutForm />)}>Log Out</button>
+      </>
+    );
   }
   
   return (
     <div id="header">
       <p><span>Members</span><span>Only</span></p>
-      <button className="button" onClick={() => openModal('login')}>Log In</button>
-      <button className="button" onClick={() => openModal('signup')}>Sign In</button>
+      {buttons()}
     </div>
   );
 }

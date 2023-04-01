526f320bcaff53be1fa9161a21ae9c983122ea65
Wantonfury <orzan.alexandru94@gmail.com>
2023-04-01 21:04:21 +0300

    Add UserContext and credentials to the post method

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,11 +2,14 @@ import '../styles/Button.css';
 import '../styles/Modal.css';
 import { useState, useContext } from "react";
 import ServerContext from '../contexts/ServerContext';
+import UserContext from '../contexts/UserContext';
 import axios from "axios";
-import BtnClose from './BtnClose';
+import '../styles/Form.css';
 
 const LoginForm = () => {
   const SERVER = useContext(ServerContext);
+  const { setUser } = useContext(UserContext);
+  const [loginError, setLoginError] = useState();
   const [loginData, setLoginData] = useState({
     username: '',
     password: ''
@@ -18,44 +21,47 @@ const LoginForm = () => {
     axios.post(`${SERVER}/users/login`, loginData, {
       headers: {
         ContentType: "multipart/form-data"
-      }
+      },
+      withCredentials: true
     })
-    .then(res => console.log(res)) // TODO: successful login
+    .catch(err => setLoginError(err.response?.data))
+    .then(res => res.data)
+    .then(res => {
+      setUser({
+        username: res.username,
+        loggedIn: true,
+        member: res.member,
+        admin: res.admin
+      });
+      
+      document.querySelector('#modal-root .btn-close').click();
+    })
     .catch(err => console.log(err));
   }
   
   const handleChange = (e) => {
     setLoginData({
       ...loginData,
-      [e.target.name]: [e.target.value]
+      [e.target.name]: e.target.value
     });
   }
   
-  const modalClose = (e) => {
-    const modalContainer = document.querySelector('#login');
-    const btnClose = document.querySelector('#login .btn-close');
-    
-    if (e.target === modalContainer || e.target === btnClose) modalContainer.style.display = 'none';
-  }
-  
   return (
-    <div id="login" className="modal-container" onClick={modalClose}>
-      <form action="" method="POST" className="modal form" onSubmit={handleSubmit}>
-        <BtnClose onClick={modalClose} />
-        
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" name="username" placeholder="Username" pattern="[A-Za-z0-9]{3,}" title="Username must  be at least 3 characters." onChange={handleChange} required />
-        </div>
-        
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" placeholder="Password" pattern="[A-Za-z0-9]{8,}" title="Password must  be at least 8 characters." onChange={handleChange} required />
-        </div>
-        
-        <button type="submit" className="button button-wide">Log In</button>
-      </form>
-    </div>
+    <form action="" method="POST" className="form" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" placeholder="Username" pattern="[A-Za-z0-9]{3,}" title="Username must  be at least 3 characters." onChange={handleChange} required />
+      </div>
+      
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" placeholder="Password" pattern="[A-Za-z0-9]{8,}" title="Password must  be at least 8 characters." onChange={handleChange} required />
+      </div>
+      
+      <button type="submit" className="button button-wide">Log In</button>
+      
+      {loginError ? <p className="form-text-error">{loginError.msg}</p> : <></>}
+    </form>
   );
 }
 

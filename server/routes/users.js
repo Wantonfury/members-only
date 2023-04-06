3319b64d76db2e2342f63f23827f79693d9af95f
Wantonfury <orzan.alexandru94@gmail.com>
2023-04-06 20:54:44 +0300

    Add membership route

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,6 +5,7 @@ const userController = require('../controllers/userController');
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
+router.post('/membership', userController.membership);
 
 router.get('/relog', userController.relog);
 

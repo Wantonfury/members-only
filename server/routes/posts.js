307162b13c63844428f4b99bc9673c2ff2867708
Wantonfury <orzan.alexandru94@gmail.com>
2023-04-06 20:54:34 +0300

    Add routes for get, post and delete

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
-//router.get('/', postController.get);
+router.get('/', postController.get_posts);
+router.post('/', postController.add_post);
+router.delete('/:id', postController.delete);
 
 module.exports = router;

ad3c7623570bb973d601d3802067dac9cdf8977a
Wantonfury <orzan.alexandru94@gmail.com>
2023-04-06 20:55:32 +0300

    Add a link to GitHub

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,13 @@
 import "../styles/Footer.css"
+import IconGithub from "../assets/images/github.png"
 
 const Footer = () => {
   return (
     <div id="footer">
-      
+      <p>Made by Wantonfury</p>
+      <a href="https://github.com/Wantonfury/members-only" target="_blank" rel="noopener noreferrer">
+        <img src={IconGithub} alt="Link to GitHub" />
+      </a>
     </div>
   );
 }
